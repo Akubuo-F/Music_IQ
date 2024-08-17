180961578835573c78cf2acc0244c9406654dc07
Akubuo-F <akubuof.work@gmail.com>
2024-08-17 20:38:40 +0200

    Update question_generator.py

--- models/question_generator.py
+++ models/question_generator.py
@@ -5,7 +5,7 @@ from utils.ai_api import AIAPI
 class QuestionGenerator:
 
     def __init__(self, ai_api: AIAPI):
-        self._ai_api: AIAPI
+        self._ai_api: AIAPI = ai_api
 
     def generate_question(self, artist_bio: str) -> Question:
         ...
