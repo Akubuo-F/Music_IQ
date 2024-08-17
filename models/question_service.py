a76c23cdd62e2e74d09449dce090d944b95bfcb6
Akubuo-F <akubuof.work@gmail.com>
2024-08-17 20:51:02 +0200

    combined the functionality of QuestionGenerator and QuestionVerifier into a single class called QuestionService

--- models/question_service.py
+++ models/question_service.py
@@ -1,11 +1,13 @@
-from models.question import Question
 from utils.ai_api import AIAPI
 
 
-class QuestionGenerator:
+class QuestionService:
 
     def __init__(self, ai_api: AIAPI):
         self._ai_api: AIAPI = ai_api
 
     def generate_questions(self, artist_bio: str, question_count: int = 3) -> list[Question]:
+        ...
+
+    def verify_question(self, artist_bio: str, question: Question) -> bool:
         ...
